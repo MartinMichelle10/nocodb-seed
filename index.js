c07e5b13c1fc034952627c5b1e148fbe47ebbf4e
Martin Michelle <martinmichelleebad@gmail.com>
2022-04-04 21:23:37 +0200

    add app .yaml

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 (async () => {
     const app = require('express')();
+    process.env['NC_DB']="pg://elwhats.postgres.database.azure.com:5432?u=mahmouds12&p=Sawsan@elmawkaa1&d=noco"
     const {Noco} = require("nocodb");
     app.use(await Noco.init({}));
     console.log(`Visit : localhost:${process.env.PORT}/dashboard`)    
