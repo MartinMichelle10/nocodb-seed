9f759070ff9bf11b83a5fdb271cf940afedeee37
Martin Michelle <martinmichelleebad@gmail.com>
2022-04-14 12:11:11 +0200

    new deployment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 (async () => {
     const app = require('express')();
-    process.env['NC_DB']="pg://elwhats.postgres.database.azure.com:5432?u=mahmouds12&p=Sawsan@elmawkaa1&d=noco"
+    process.env['NC_DB']="pg://34.65.205.61:5432?u=postgres&p=postgres&d=nocodb"
     const {Noco} = require("nocodb");
     app.use(await Noco.init({}));
     console.log(`Visit : localhost:${process.env.PORT}/dashboard`)    
