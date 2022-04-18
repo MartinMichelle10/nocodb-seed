20dbf2c0ca43b8e402980905c51ae1e135658a4d
Martin Michelle <martinmichelleebad@gmail.com>
2022-04-18 19:31:50 +0200

    add env config

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 (async () => {
+    require('dotenv').config()
     const app = require('express')();
-    process.env['NC_DB']="pg://34.65.205.61:5432?u=postgres&p=postgres&d=nocodb"
-    const {Noco} = require("nocodb");
+    // process.env['NC_DB']="pg://34.65.205.61:5432?u=postgres&p=postgres&d=nocodb"
+    console.log(process.env['NC_DB'])
+    const { Noco } = require("nocodb");
     app.use(await Noco.init({}));
-    console.log(`Visit : localhost:${process.env.PORT}/dashboard`)    
+    console.log(`Visit : localhost:${process.env.PORT}/dashboard`)
     app.listen(process.env.PORT);
 })()
